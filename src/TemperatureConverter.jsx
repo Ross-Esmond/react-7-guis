fe7e3757cfe899d59d772a6257e93b66cc785bb7
Ross Esmond <r.esmond@protonmail.com>
2022-05-25 10:59:17 -0500

    feat: added error and NaN value handling to useInput
    If the parser throws an error useInput will console log the error but
    will continue to function. It will just use the last input value for the
    time being until a parse succeeds, and then will function as normal.
    
    NaN values are never considered equal. This is usually a good things, as
    it keeps mathematical systems from thinking two illegal operations are
    equal, `5/0 = Math.asin(1.1)`, but for useInput, I only care if the
    higher-level component passed back the parsed value verbatim, not
    technicalities of mathematics. So useInput will now treat NaN values as
    being equal, and passing back down a NaN value works as intended.

--- src/TemperatureConverter.jsx
+++ src/TemperatureConverter.jsx
@@ -6,13 +6,13 @@ export default function TemperatureConverter ({}) {
     const [temp, setTemp] = useState(0)
     const [celsius, setCelsius] = useInput(
         temp, t => setTemp(t),
-        t => t.toString(),
+        (t, s) => isNaN(t) ? s : t.toString(),
         c => parseFloat(c)
     )
     const [fahrenheit, setFahrenheit] = useInput(
-        celsius, c => setCelsius(c),
-        c => Math.round(parseFloat(c)*(9/5) + 32).toString(),
-        f => Math.round((parseFloat(f) - 32) * (5/9)).toString()
+        temp, t => setTemp(t),
+        (t, s) => isNaN(t) ? s : Math.round(t*(9/5) + 32).toString(),
+        f => Math.round((parseFloat(f) - 32) * (5/9))
     )
 
     return (
--- src/useInput.js
+++ src/useInput.js
@@ -1,18 +1,39 @@
 import { useRef } from 'react'
 
+function shouldRetainPriorInput({ state, parsed }, high) {
+    if (state === 'error') return true
+    if (high === parsed) return true
+    if (isNaN(parsed) && isNaN(high)) return true
+}
+
 export default function useInput(high, onChange, display, parse) {
-    const cache = useRef(null)
+    const cache = useRef({
+        state: 'parsed',
+        parsed: high,
+        low: display(high)
+    })
 
     function handleChange(low) {
-        const parsed = parse(low)
-        cache.current = {
-            parsed,
-            low
+        let parsed = null
+        try {
+            parsed = parse(low)
+            cache.current = {
+                state: 'parsed',
+                parsed,
+                low
+            }
+        } catch (err) {
+            console.error(`The parser for the input you just edited threw an error with input ${low}.`)
+            cache.current = {
+                state: 'error',
+                low
+            }
+        } finally {
+            onChange(parsed)
         }
-        onChange(parsed)
     }
 
-    if (cache.current != null && high === cache.current.parsed) {
+    if (shouldRetainPriorInput(cache.current, high)) {
         return [cache.current.low, handleChange]
     } else {
         return [display(high), handleChange]
